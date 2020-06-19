ea1669fcb501f353bb34a5d07beeb961f4685d03
ashishiscool <ashishissupercool@gmail.com>
2020-06-19 12:13:32 +0530

    Add files via upload

---
+++ Shopping list/public_html/shop.js
@@ -0,0 +1,28 @@
+var button= document.getElementById("enter");
+var input=document.getElementById("userinput");
+var ul=document.querySelector("ul");
+
+function createList(){
+        var li=document.createElement("li");
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        input.value="";    
+}
+
+button.addEventListener("click",function(){
+    if(input.value.length>0){
+    createList();
+    }
+})
+
+input.addEventListener("keypress",function(event){
+     if(input.value.length>0 && event.keyCode===13){
+        createList();
+     }
+})
+
+ul.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('done');
+  }
+});
